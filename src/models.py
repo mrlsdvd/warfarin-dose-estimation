2d4c5e4d83cbc1e1c3a6015a0d90510ad89745f1
mrlsdvd <mrlsdvd@stanford.edu>
2019-03-09 03:45:03 -0800

    Update models to handle some of processing  pipeline

--- src/models.py
+++ src/models.py
@@ -1,17 +1,54 @@
 import numpy as np
 import pandas as pd
+from utils import bin_predictions
 
-class DosageBaseline():
+class DosageModel():
+    """
+    Abstract class for dosage models.
+    """
+
     def __init__(self):
         pass
 
-    def predict(self, X):
+    def get_features(self, data):
+        """
+        Extracts necessary feature columns for model from data matrix.
+
+        Arguments:
+            data (pd.DataFrame): Data matrix
+
+        Returns:
+            features (pd.DataFrame): Feature matrix
+        """
+        pass
+
+    def get_targets(self, data):
+        """
+        Extracts targets (labels) from data matrix.
+
+        Arguments:
+            data (pd.DataFrame): Data matrix
+
+        Returns:
+            targets (pd.DataFrame): Target matrix
+        """
+        pass
+
+    def predict(self):
+        pass
+
+class DosageBaseline(DosageModel):
+    def __init__(self):
+        pass
+
+    def predict(self, X, bin=True):
         """
         Estimate the square root of weekly warfarin dose.
 
         Arguments:
             X (np.array): Batch of inputs of shape (N, D), where D is the
                 number of features (those defined above)
+            bin (bool): Whether to bin the predictions into dosage categories
 
         Returns:
             Y (np.array): Batch of square root weekly dose estimates of
@@ -22,6 +59,8 @@ class DosageBaseline():
         ones = np.ones((N, 1))
         offset_data = np.hstack([ones, X])
         Y = np.dot(offset_data, self.weights)
+        if bin:
+            Y = bin_predictions(Y)
         return Y
 
 class PDABaseline(DosageBaseline):
@@ -31,7 +70,7 @@ class PDABaseline(DosageBaseline):
         Age in decades (int in [0,9])
         Height in cm (float)
         Weight in kg (float)
-        VKORC1^ A/G (int either 1 or 0)
+        VKORC1 A/G (int either 1 or 0)
         VKORC1 A/A (int either 1 or 0)
         VKORC1 genotype unknown (int either 1 or 0)
         CYP2C9 *1/*2 (int either 1 or 0)
@@ -48,6 +87,29 @@ class PDABaseline(DosageBaseline):
     """
 
     def __init__(self):
+        col_names = [
+            "Age",
+            "Height_(cm)",
+            "Weight_(kg)",
+            "VKORC1_A/G",
+            "VKORC1_A/A",
+            "VKORC1_nan",
+            "Cyp2C9_*1/*2",
+            "Cyp2C9_*1/*3",
+            "Cyp2C9_*2/*2",
+            "Cyp2C9_*2/*3",
+            "Cyp2C9_*3/*3",
+            "Cyp2C9_nan",
+            "Race_Asian",
+            "Race_Black_or_African_American",
+            "Race_nan",
+            "enzyme_inducer_status",
+            "amiodarone_status"
+        ]
+        self.col_names = col_names
+
+        self.target_name = 'Therapeutic_Dose_of_Warfarin'
+
         weights = [
             5.6044,     # Offset
             -0.2614,    # Age
@@ -68,8 +130,13 @@ class PDABaseline(DosageBaseline):
             1.1816,     # Enzyme inducer
             -0.5503     # Amiodarone
         ]
-
         self.weights = np.array(weights)
+
+    def get_features(self, data):
+        return data[self.col_names]
+
+    def get_targets(self, data):
+        return data[[self.target_name]]
 
 
 class CDABaseline(DosageBaseline):
@@ -87,6 +154,20 @@ class CDABaseline(DosageBaseline):
     """
 
     def __init__(self):
+        col_names = [
+            "Age",
+            "Height_(cm)",
+            "Weight_(kg)",
+            "Race_Asian",
+            "Race_Black_or_African_American",
+            "Race_nan",
+            "enzyme_inducer_status",
+            "amiodarone_status"
+        ]
+        self.col_names = col_names
+
+        self.target_name = 'Therapeutic_Dose_of_Warfarin'
+
         weights = [
             4.0376,     # Offset
             -0.2546,    # Age
@@ -101,6 +182,13 @@ class CDABaseline(DosageBaseline):
 
         self.weights = np.array(weights)
 
+    def get_features(self, data):
+        return data[self.col_names]
+
+    def get_targets(self, data):
+        return data[[self.target_name]]
+
+
 class FDBaseline(DosageBaseline):
     """
     Implements fixed dosage baseline.
@@ -110,6 +198,13 @@ class FDBaseline(DosageBaseline):
 
     def __init__(self, dosage):
         self.dosage = dosage
+        self.target_name = 'Therapeutic_Dose_of_Warfarin'
+
+    def get_features(self, data):
+        return data.drop(self.target_name, axis=1)  # Remove target from data
+
+    def get_targets(self, data):
+        return data[[self.target_name]]
 
     def predict(self, X):
         N, D = X.shape
