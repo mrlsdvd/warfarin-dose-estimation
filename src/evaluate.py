0f17cb27d5f9500b88b66943463a8b03e7abd08f
Minh-An Quinn <minhanquinn@DN0a25b457.SUNet>
2019-03-11 14:24:52 -0700

    regret for baseline models

--- src/evaluate.py
+++ src/evaluate.py
@@ -13,7 +13,7 @@ class Evaluator():
     def __init__(self, model):
         self.model = model
 
-    def accuracy(self, data, actual):
+    def accuracy(self, predicted, actual):
         """
         Compute accuracy score on given data.
 
@@ -24,5 +24,24 @@ class Evaluator():
         Returns:
             accuracy (float): Accuracy score
         """
-        predicted = self.model.predict(data)
         return accuracy_score(actual, predicted)
+    
+    def regret(self, predicted, actual):
+        regret = np.zeros((predicted.shape[0]+1, ))
+        total_regret = 0
+        for i in range(predicted.shape[0]):
+            if predicted[i] != actual[i]:
+                total_regret += 1
+            regret[i+1] = total_regret
+        return regret
+    
+    def incorrect_decisions(self, predicted, actual):
+        incorrect_decisions = np.zeros(predicted.shape)
+        num_incorrect_decisions = 0
+        for i in range(predicted.shape[0]):
+            if predicted[i] != actual[i]:
+                num_incorrect_decisions += 1
+            incorrect_decisions[i] = num_incorrect_decisions/(i+1)
+        return incorrect_decisions
+            
+        
