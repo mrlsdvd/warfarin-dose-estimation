30a93636dcb847c118b8ef39c5e001bce9c5188f
mrlsdvd <mrlsdvd@stanford.edu>
2019-03-16 03:38:17 -0700

    Add construct_forced_samples to utils

--- src/utils.py
+++ src/utils.py
@@ -24,3 +24,30 @@ def bin_predictions(predictions):
     daily_dosage[daily_dosage > 7] = 2
 
     return daily_dosage
+
+def construct_forced_samples(N, K, q, T):
+    """
+    Constructs forced samples for each arm.
+    T[i, t] == 1 if arm i is forced sampled for time / sample t,
+    otherwise, T[i, t] == 0.
+
+    Arguments:
+        N (int): Number of timesteps / samples
+        K (int): Number of arms / actions
+        q (int): Sampling parameter
+        T (np.array): Numpy array of shape (K, N) filled with zeros
+    """
+    for i in range(K):
+        # n = {0, 1, 2, 3, 4, ...}
+        n_idxs = np.arange(q)
+        # j = {q*i, q*i + 1, q*i + 2, ..., q*i}
+        j_idxs = np.arange(q*i, q*(i+1))
+        # sampled indices = (2^n - 1)*Kq + j
+        idxs = np.array([np.power(2, n_idxs[n])-1 for n in n_idxs])
+        idxs = idxs * (K*q) + j_idxs
+        # Remove out of bound indices
+        idxs = idxs[idxs < N]
+
+        T[i, idxs] = 1
+
+    return T
